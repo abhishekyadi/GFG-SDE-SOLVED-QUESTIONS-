708882297252d66845380481e552959d9b1322a3
ABHISHEK KUMAR <97779584+abhishekyadi@users.noreply.github.com>
2023-04-12 23:39:34 +0530

    Added solution - LeetHub

---
+++ Java Basic Data Types - GFG/java-basic-data-types.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            Solution ob = new Solution();
+            System.out.println(ob.javaIntType(sc));
+            System.out.println(ob.javaStringType(sc));
+            System.out.println(ob.javaFloatType(sc));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    int javaIntType(Scanner sc) {
+     return sc.nextInt();
+    }
+    
+    String javaStringType(Scanner sc) {
+        return sc.next();
+    }
+    
+    float javaFloatType(Scanner sc) {
+        return sc.nextFloat();
+    }
+};
